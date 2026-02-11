9ecc654aa52ddf1188bac11285d985f94316e15a
ehsan Qanda <qanda6994@gmail.com>
2026-02-11 15:53:32 +0330

    create app esab

--- config/settings.py
+++ config/settings.py
@@ -115,9 +115,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = 'static/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
  
 # castom models my 
+
 PATH_USER_MODEL = "esabs.CustomUser"
--- esabs/admin.py
+++ esabs/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserChangeForm,CustomUserCreationForm
-from .models import CustomUser
+# from .forms import CustomUserChangeForm,CustomUserCreationForm
+# from .models import CustomUser
 
-class AdminSite(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
+# class AdminSite(UserAdmin):
+#     add_form = CustomUserCreationForm
+#     form = CustomUserChangeForm
+#     model = CustomUser
 
-admin.site.register(CustomUser,AdminSite)
+# admin.site.register(CustomUser,AdminSite)
 
